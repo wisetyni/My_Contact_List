71b9a986a504c8f8bdbdc0684bd65a84a4c4aadc
Yvonne Allen <yallen011@gmail.com>
2018-04-15 15:50:02 -0400

    Added ContactActivity and moves relavent code from ContactListActivity to ContactActivity

---
+++ app/src/main/java/mycontactlist/example/com/my_contact_list/ContactActivity.java
@@ -0,0 +1,113 @@
+package mycontactlist.example.com.my_contact_list;
+
+import android.arch.lifecycle.ViewModelProviders;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.telephony.PhoneNumberFormattingTextWatcher;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class ContactActivity extends AppCompatActivity {
+
+    private TextView mTextMessage, birthday, save;
+    private ImageView mCalendar;
+    private Contact mCurrentContact;
+    private EditText name, streetAddress, city, zipCode, homePhone, cellPhone, email, state;
+    private RelativeLayout relativeLayout;
+    private SharedPreferences mSharedPreferences;
+    private Toolbar toolbar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact);
+
+        mCurrentContact = new Contact();
+        final ContactViewModel contactViewModel = ViewModelProviders.of(this).get(ContactViewModel.class);
+        initializeViews();
+        initTextChangedEvents();
+
+        relativeLayout.setBackgroundColor(Color.parseColor(mSharedPreferences.getString("background_color", "#111111")));
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        mCalendar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DialogFragment bdayPickerFragment = new BirthdayPickerFragment();
+                bdayPickerFragment.show(getSupportFragmentManager(), "birthdayPicker");
+            }
+        });
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCurrentContact.setContactName(name.getText().toString());
+                mCurrentContact.setPhoneNumber(homePhone.getText().toString());
+                mCurrentContact.setCellNumber(cellPhone.getText().toString());
+                mCurrentContact.setCity(city.getText().toString());
+                mCurrentContact.setState(state.getText().toString());
+                mCurrentContact.setStreetAddress(streetAddress.getText().toString());
+                mCurrentContact.setZipCode(zipCode.getText().toString());
+                mCurrentContact.setEmail(email.getText().toString());
+                contactViewModel.saveContact(mCurrentContact);
+                hideKeyboard();
+            }
+        });
+    }
+
+    private void initializeViews() {
+
+        relativeLayout = findViewById(R.id.container);
+        toolbar = findViewById(R.id.toolbar);
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        name = findViewById(R.id.editName);
+        homePhone = findViewById(R.id.editHomePhone);
+        cellPhone = findViewById(R.id.editCell);
+        city = findViewById(R.id.editCity);
+        streetAddress  = findViewById(R.id.editAddress);
+        state = findViewById(R.id.editState);
+        zipCode = findViewById(R.id.editZipCode);
+        email = findViewById(R.id.editEMail);
+        birthday = findViewById(R.id.textBirthday);
+        save = findViewById(R.id.save);
+        mCalendar = findViewById(R.id.img_bday_calendar);
+
+        //bottom nav
+        mTextMessage = findViewById(R.id.message);
+
+    }
+
+    private void initTextChangedEvents() {
+
+        homePhone.addTextChangedListener(new PhoneNumberFormattingTextWatcher());
+        cellPhone.addTextChangedListener(new PhoneNumberFormattingTextWatcher());
+    }
+
+    private void hideKeyboard() {
+        InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(name.getWindowToken(), 0);
+        imm.hideSoftInputFromWindow(streetAddress.getWindowToken(), 0);
+        imm.hideSoftInputFromWindow(city.getWindowToken(), 0);
+        imm.hideSoftInputFromWindow(state.getWindowToken(), 0);
+        imm.hideSoftInputFromWindow(zipCode.getWindowToken(), 0);
+        imm.hideSoftInputFromWindow(homePhone.getWindowToken(), 0);
+        imm.hideSoftInputFromWindow(cellPhone.getWindowToken(), 0);
+        imm.hideSoftInputFromWindow(email.getWindowToken(), 0);
+    }
+}
--- app/src/main/java/mycontactlist/example/com/my_contact_list/ContactListActivity.java
+++ app/src/main/java/mycontactlist/example/com/my_contact_list/ContactListActivity.java
@@ -1,37 +1,18 @@
 package mycontactlist.example.com.my_contact_list;
 
 import android.arch.lifecycle.ViewModelProviders;
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.telephony.PhoneNumberFormattingTextWatcher;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
 
 public class ContactListActivity extends AppCompatActivity {
 
-    private TextView mTextMessage, birthday, save;
-    private ImageView mCalendar;
-    private Contact mCurrentContact;
-    private EditText name, streetAddress, city, zipCode, homePhone, cellPhone, email, state;
+
     private BottomNavigationView navigation;
-    private RelativeLayout relativeLayout;
-    private SharedPreferences mSharedPreferences;
-    private Toolbar toolbar;
+    private Contact mCurrentContact;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -44,7 +25,7 @@ public class ContactListActivity extends AppCompatActivity {
                     startActivity(contact_intent);
                     return true;
                 case R.id.navigation_dashboard:
-                    Intent dashboard_intent = new Intent(getApplicationContext(), ContactListActivity.class);
+                    Intent dashboard_intent = new Intent(getApplicationContext(), ContactMapActivity.class);
                     startActivity(dashboard_intent);
                     return true;
                 case R.id.navigation_settings:
@@ -65,76 +46,16 @@ public class ContactListActivity extends AppCompatActivity {
         final ContactViewModel contactViewModel = ViewModelProviders.of(this).get(ContactViewModel.class);
 
         initializeViews();
-        initTextChangedEvents();
 
-        relativeLayout.setBackgroundColor(Color.parseColor(mSharedPreferences.getString("background_color", "#111111")));
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-        mCalendar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                DialogFragment bdayPickerFragment = new BirthdayPickerFragment();
-                bdayPickerFragment.show(getSupportFragmentManager(), "birthdayPicker");
-            }
-        });
 
-        save.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mCurrentContact.setContactName(name.getText().toString());
-                mCurrentContact.setPhoneNumber(homePhone.getText().toString());
-                mCurrentContact.setCellNumber(cellPhone.getText().toString());
-                mCurrentContact.setCity(city.getText().toString());
-                mCurrentContact.setState(state.getText().toString());
-                mCurrentContact.setStreetAddress(streetAddress.getText().toString());
-                mCurrentContact.setZipCode(zipCode.getText().toString());
-                mCurrentContact.setEmail(email.getText().toString());
-                contactViewModel.saveContact(mCurrentContact);
-                hideKeyboard();
-            }
-        });
     }
 
     private void initializeViews() {
-        relativeLayout = findViewById(R.id.container);
-        toolbar = findViewById(R.id.toolbar);
-        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-        name = findViewById(R.id.editName);
-        homePhone = findViewById(R.id.editHomePhone);
-        cellPhone = findViewById(R.id.editCell);
-        city = findViewById(R.id.editCity);
-        streetAddress  = findViewById(R.id.editAddress);
-        state = findViewById(R.id.editState);
-        zipCode = findViewById(R.id.editZipCode);
-        email = findViewById(R.id.editEMail);
-        birthday = findViewById(R.id.textBirthday);
-        save = findViewById(R.id.save);
-        mCalendar = findViewById(R.id.img_bday_calendar);
 
-        //bottom nav
-        mTextMessage = findViewById(R.id.message);
         navigation = findViewById(R.id.navigation);
-    }
-
-    private void initTextChangedEvents() {
-
-        homePhone.addTextChangedListener(new PhoneNumberFormattingTextWatcher());
-        cellPhone.addTextChangedListener(new PhoneNumberFormattingTextWatcher());
-    }
-
-    private void hideKeyboard() {
-        InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
-        imm.hideSoftInputFromWindow(name.getWindowToken(), 0);
-        imm.hideSoftInputFromWindow(streetAddress.getWindowToken(), 0);
-        imm.hideSoftInputFromWindow(city.getWindowToken(), 0);
-        imm.hideSoftInputFromWindow(state.getWindowToken(), 0);
-        imm.hideSoftInputFromWindow(zipCode.getWindowToken(), 0);
-        imm.hideSoftInputFromWindow(homePhone.getWindowToken(), 0);
-        imm.hideSoftInputFromWindow(cellPhone.getWindowToken(), 0);
-        imm.hideSoftInputFromWindow(email.getWindowToken(), 0);
     }
 
 }
