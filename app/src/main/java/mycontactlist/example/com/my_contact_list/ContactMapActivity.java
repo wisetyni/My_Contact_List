2d142196eabf75dc16923561fe5abf955aca4276
Yvonne Allen <yallen011@gmail.com>
2018-04-18 20:55:50 -0400

    added compass to the toolbar

--- app/src/main/java/mycontactlist/example/com/my_contact_list/ContactMapActivity.java
+++ app/src/main/java/mycontactlist/example/com/my_contact_list/ContactMapActivity.java
@@ -5,14 +5,15 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Point;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
-import android.location.LocationManager;
-import android.location.LocationListener;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -24,11 +25,8 @@ import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.Display;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -56,10 +54,13 @@ public class ContactMapActivity extends FragmentActivity implements OnMapReadyCa
     private static final int MY_PERMISSIONS_REQUEST_LOCATION = 1;
     private GoogleMap mGoogleMap;
     private BottomNavigationView navigation;
-    private RelativeLayout mapContainer;
     private ArrayList<Contact> mContacts;
     private Contact currentContact;
     private FusedLocationProviderClient mFusedLocationClient;
+    SensorManager sensorManager;
+    Sensor accelerometer;
+    Sensor magnetometer;
+    TextView textDirection;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = NavUtil.getBottomNav(this);
@@ -74,10 +75,8 @@ public class ContactMapActivity extends FragmentActivity implements OnMapReadyCa
         initLocation();
         initMapType();
         initBottomNavigation();
-
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.contactMap);
-        mapFragment.getMapAsync(this);
+        initSensors();
+        initSupportMapFragment();
 
         mContacts = new ArrayList<>();
         currentContact = null;
@@ -94,6 +93,26 @@ public class ContactMapActivity extends FragmentActivity implements OnMapReadyCa
             ds.close();
         }
 
+    }
+
+    private void initSupportMapFragment() {
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.contactMap);
+        mapFragment.getMapAsync(this);
+    }
+
+    private void initSensors() {
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        magnetometer = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+
+        if (accelerometer != null && magnetometer != null) {
+            sensorManager.registerListener(mySensorEventListener, accelerometer, SensorManager.SENSOR_DELAY_FASTEST);
+            sensorManager.registerListener(mySensorEventListener, magnetometer, SensorManager. SENSOR_DELAY_FASTEST);
+        } else {
+            Toast.makeText(this, "Sensors not found", Toast.LENGTH_LONG).show();          //3
+        }
+        textDirection = (TextView) findViewById(R.id.textHeading);
     }
 
     private void initBottomNavigation() {
@@ -321,4 +340,41 @@ public class ContactMapActivity extends FragmentActivity implements OnMapReadyCa
             }
         }
     }
+
+    private SensorEventListener mySensorEventListener = new SensorEventListener() {
+
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {  }
+
+        float[] accelerometerValues;
+        float[] magneticValues;
+
+        public void onSensorChanged(SensorEvent event) {
+            if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER)
+                accelerometerValues = event.values;
+            if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD)
+                magneticValues = event.values;
+            if (accelerometerValues!= null && magneticValues!= null) {
+                float R[] = new float[9];
+                float I[] = new float[9];
+                boolean success = SensorManager.getRotationMatrix(R, I, accelerometerValues, magneticValues);
+                if (success) {
+                    float orientation[] = new float[3];
+                    SensorManager.getOrientation(R, orientation);
+
+                    float azimut = (float) Math.toDegrees(orientation[0]);
+                    if (azimut < 0.0f) { azimut+=360.0f;}
+                    String direction;
+                    if (azimut >= 0 && azimut < 45) { direction = "N"; }
+                    else if (azimut >= 45 && azimut < 90) { direction = "NE"; }
+                    else if (azimut >= 90 && azimut < 135) { direction = "E"; }
+                    else if (azimut >= 135 && azimut < 180) { direction = "SE"; }
+                    else if (azimut >= 180 && azimut < 225) { direction = "S"; }
+                    else if (azimut >= 225 && azimut < 270) { direction = "SW"; }
+                    else if(azimut >= 270 && azimut < 315 ) { direction = "W"; }
+                    else { direction = "NW"; }
+                    textDirection.setText(direction);
+                }
+            }
+        }
+    };
 }
